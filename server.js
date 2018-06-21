a6f886b6682243087b1caa876bd8104e7e5deadb
Rafael Fraga <rafraga@gmail.com>
2018-06-20 20:08:21 -0700

    changes

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors');
 // const domtoimage = require('dom-to-image')
 const app = express()
 const viewsDir = path.join(__dirname, 'views')
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.sendFile(path.join(viewsDir, 'faceDetectionVideo.html'))
+});
 
- app.all('/face_detection_video_dev', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
+// app.get('/', function (req, res) { 
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.sendFile(path.join(viewsDir, 'faceDetectionVideo.html'))
+// });
+
+app.use(cors()); 
+app.use(express.static(path.join(__dirname, '../')));
 
 app.use(express.static(viewsDir))
 app.use(express.static(path.join(__dirname, './public')))
@@ -22,10 +25,6 @@ app.use(express.static(path.join(__dirname, './weights')))
 app.use(express.static(path.join(__dirname, './dist')))
 app.use(express.static(path.join(__dirname, './node_modules/axios/dist')))
 app.use(express.static(path.join(__dirname, './node_modules/dom-to-image/dist')))
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(viewsDir, 'faceDetectionVideo.html'))
-});
 
 app.get('/dev', (req, res) => res.redirect('/face_detection_video_dev'))
 app.get('/face_detection_video_dev', (req, res) => res.sendFile(path.join(viewsDir, 'faceDetectionVideo_dev01.html')))
