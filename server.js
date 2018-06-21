1254b82f37c5d494d5d68486605e604b156b0539
Rafael Fraga <rafraga@gmail.com>
2018-06-20 21:19:57 -0700

    changes

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const path = require('path')
 const app = express()
 
 const viewsDir = path.join(__dirname, 'views')
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use(express.static(viewsDir))
 app.use(express.static(path.join(__dirname, './public')))
 app.use(express.static(path.join(__dirname, './weights')))
