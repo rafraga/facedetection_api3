a13bff10dd7946c308cb2b4ae90fe62c8deecfe3
Rafael Fraga <rafraga@gmail.com>
2018-06-21 18:47:45 -0700

    changes

--- dist/facedetection_js_dev01.js
+++ dist/facedetection_js_dev01.js
@@ -105,15 +105,10 @@
           //   });
 
 
-          var divContents = $("#faces_view").html()
-          var printWindow = window.open('', '', 'height=400,width=800')
-          printWindow.document.write('<html><head><title>Faces</title>')
-          printWindow.document.write('</head><body >')
-          printWindow.document.write(divContents)
-          printWindow.document.write('</body></html>')
-          printWindow.document.close()
-          printWindow.print()
-
+          html2canvas(document.querySelector("#faces_view")).then(canvas => {
+            document.body.appendChild(canvas)
+          });
+          
           return results_data
        
 
